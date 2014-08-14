bd1f0e962d60e6b9c6f43248331281297ffe9b47
Charisse Lambert <charisse8@gmail.com>
2014-08-14 16:43:53 -0500

    fixed categories

--- app/controllers/recipes.js
+++ app/controllers/recipes.js
@@ -4,6 +4,7 @@ var Recipe = require('../models/recipe');
 
 exports.index = function(req, res){
   Recipe.all(function(err, recipes){
+    console.log(recipes);
     res.render('recipes/index', {recipes:recipes});
   });
 };
